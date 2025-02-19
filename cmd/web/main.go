dd43715d877c896f92efc16272209bfcd38eb1be
Serhii Zakutko <sergeyzakutko2000@gmail.com>
2025-02-19 13:34:07 +0100

    Repository pattern was implemented

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"fmt"
 	"go-greeders/models"
@@ -16,7 +15,6 @@ const port = ":4000"
 type application struct {
 	tempalateMap map[string]*template.Template
 	config       appConfig
-	DB           *sql.DB
 	Models       models.Models
 }
 
@@ -40,7 +38,6 @@ func main() {
 		log.Panic(err)
 	}
 
-	app.DB = db
 	app.Models = *models.New(db)
 
 	srv := &http.Server{
